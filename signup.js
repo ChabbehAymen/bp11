f0e64194d2d089b97a36bf2cc0349f9b22106e43
devaymen15.5.3@gmail.com <devaymen15.5.3@gmail.com>
2024-01-17 23:59:13 +0100

    deleted:    signup.js

--- signup.js
+++
@@ -1,5 +0,0 @@
-
-console.log(document.querySelector('input[name="error-controler"]'));
-document.querySelector('input[name="error-controler"]').addEventListener('change', e =>{
-    console.log(e.target.value);
-});
